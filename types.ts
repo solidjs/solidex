2ca83ea496d3266ecdd142becc41b3d2af103f7f
Erik Demaine <edemaine@mit.edu>
2022-06-24 14:06:34 -0400

    Replace enums with objects + types, split resource vs. package

--- types.ts
+++ types.ts
@@ -1,31 +1,44 @@
-export enum ResourceType {
-  Article = 'article',
-  Video = 'video',
-  Podcast = 'podcast',
-  Library = 'library',
-  Package = 'package',
+export const ResourceTypes = {
+  'article': 1,
+  'video': 1,
+  'podcast': 1,
+};
+export type ResourceType = keyof typeof ResourceTypes;
+
+export const PackageTypes = {
+  'package': 1,
+};
+export type PackageType = keyof typeof PackageTypes;
+
+export const EcosystemTypes = {...ResourceTypes, ...PackageTypes};
+export type EcosystemType = keyof typeof EcosystemTypes;
+
+export const ResourceCategories = {
+  'primitive': 1,
+  'router': 1,
+  'data': 1,
+  'ui': 1,
+  'plugin': 1,
+  'starters': 1,
+  'build_utility': 1,
+  'add_on': 1,
+  'testing': 1,
+  'educational': 1,
 }
-export enum ResourceCategory {
-  Primitives = 'primitive',
-  Routers = 'router',
-  Data = 'data',
-  UI = 'ui',
-  Plugins = 'plugin',
-  Starters = 'starters',
-  BuildUtilities = 'build_utility',
-  AddOn = 'add_on',
-  Testing = 'testing',
-  Educational = 'educational',
-}
-export interface Resource {
+export type ResourceCategory = keyof typeof ResourceCategories;
+
+export interface Ecosystem {
   title: string;
   link: string;
   author?: string;
   author_url?: string;
   description?: string;
-  type: ResourceType;
+  type: EcosystemType;
   categories: readonly ResourceCategory[];
   official?: boolean; // If the resource is an official Solid resource
   keywords?: readonly string[];
   published_at?: number;
 }
+
+export type Package = Ecosystem & {type: PackageType};
+export type Resource = Ecosystem & {type: ResourceType};
